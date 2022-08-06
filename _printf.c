e32ea98caeeda645a74552dbd13361bffea6e3dd
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-06 13:39:59 +0000

    Adding the header files for our _printf function

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - A function that is similar to standard printf function
+ *
+ * @format: Character string
+ *
+ * @...: Other arguments
+ *
+ * Return: Returns the number of characters printed
+ */
+
