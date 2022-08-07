7695cacf7ae56ccbac5f965d1af9f8a47dffdccd
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-07 15:40:45 +0000

    Addind an edge case where null argument is passed to our printf

--- _printf.c
+++ _printf.c
@@ -1,54 +1,82 @@
-#include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+void _putchar_c(char c)
+{
+	write(1, &c, 1);
+}
+
+void print_str(va_list a)
+{
+	char *c;
+
+	c = va_arg(a, char *);
+	while (*c != '\0')
+	{
+		_putchar_c(*c++);
+	}
+}
+void _putchar(va_list a)
+{
+	char c;
+
+	c = va_arg(a, int);
+	write(1, &c, 1);
+}
+void print_int(void)
+{
+
+}
+
 
 /**
- * _printf - A function that is similar to standard printf function
- *
- * @format: Character string
- *
- * @...: Other arguments
- *
- * Return: Returns the number of characters printed
+ * _printf - prints output according to a format.
+ * @format: input string.
+ * Return: int, number of characters printed,
  */
 
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	va_list arg;
-	int i, displayed = 0;
-	char *str;
+	va_list a;
+	int i, j, count;
 
-	va_start(arg, format);
-
+	cs_t cspec[] = {
+		{'c', _putchar},
+		{'s', print_str},
+		{'d', print_int},
+		{'i', print_int}
+	};
 
 	if (format == NULL)
 		return (0);
-	for (i = 0; format[i] != '\0'; i++)
+
+	i = j = count = 0;
+	va_start(a, format);
+	while (format[i])
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			_putchar(format[i]);
-			displayed++;
+			j = 0;
+			while (j < 4)
+			{
+				if (format[i + 1] == cspec[j].cs &&
+				    format[i + 1] != '%')
+				{
+					cspec[j].f(a);
+					i++;
+				}
+				j++;
+			}
+			i++;
+			_putchar_c(format[i]);
 		}
 		else
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					_putchar(va_arg(arg, int));
-					displayed++;
-					break;
-				case 's':
-					str = va_arg(arg, char *);
-					displayed = displayed + print_str(str);
-					break;
-				case '%':
-					_putchar('%');
-					displayed++;
-					break;
-			}
-		}
+			_putchar_c(format[i]);
+		count++;
+		i++;
+
 	}
-	va_end(arg);
-	return (displayed);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,8 +1,14 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+typedef struct Cspecs {
+		char cs;
+			void (*f)();
+} cs_t;
 
-int print_str(char *str);
+void _putchar_c(char c);
+void _putchar(va_list a);
+void print_str(va_list a);
 int _printf(const char *format, ...);
-int _putchar(char c);
 
 #endif
