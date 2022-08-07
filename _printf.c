0e95cb62d52c641c2d4104e72fb76b4feba2c15b
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-07 14:14:09 +0000

    Adding format specifiers %c, %s and %% to our _printf function

--- _printf.c
+++ _printf.c
@@ -11,20 +11,41 @@
  * Return: Returns the number of characters printed
  */
 
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
 	va_list arg;
-	int i = 0;
-	int displayed;
+	int i, k = 0, var, displayed = 0;
+	char *str;
 
 	va_start(arg, format);
-	while (format[i] != '\0')
-	{
-		_putchar(format[i]);
-		displayed += 1;
-		i++;
-	}
 
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			displayed++;
+		}
+		else
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(arg, int));
+					displayed++;
+					break;
+				case 's':
+					str = va_arg(arg, char *);
+					displayed = displayed + print_str(str);
+					break;
+				case '%':
+					_putchar('%');
+					displayed++;
+					break;
+			}
+		}
+	}
 	va_end(arg);
 	return (displayed);
 }
