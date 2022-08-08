a99669bee323fb9c385b9bb0029f695bd4f75093
Confidence <guzorochinwankwo@gmail.com>
2022-08-08 04:26:17 +0000

    Added more format specifiers

--- _printf.c
+++ _printf.c
@@ -2,46 +2,83 @@
 #include <stdarg.h>
 
 /**
- * _printf - A function that works like standard printf function
- * @format: String to print
- * @...: Other arguments
- * Return: Number of characters displayed
+ * check_format - checks if there is a valid format specifier
+ * @format: possible valid format specifier
+ * Return: pointer to valid function or NULL
  */
+int (*check_format(const char *format))(va_list)
+{
+	int i = 0;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"b", print_b},
+		{"u", print_u},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
+	for (; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+			break;
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - function for format printing
+ * @format: list of arguments to printing
+ * Return: Number of characters to printing
+ */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int i = 0, displayed = 0;
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, counter = 0;
 
-	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 
-	while  (format[i] != '\0')
+	va_start(ap, format);
+	while (format && format[i])
 	{
 		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			displayed++;
+			counter++;
+			i++;
+			continue;
 		}
 		else
 		{
-			if (format[i + 1] == 'c')
+			if (format[i + 1] == '%')
 			{
-				_putchar(va_arg(arg, int));
-				displayed++, i++;
+				_putchar('%');
+				counter++;
+				i += 2;
+				continue;
 			}
-			else if (format[i + 1] == 's')
+			else
 			{
-				displayed += print_str(va_arg(arg, char *));
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				_putchar(format[i]);
-				displayed++, i++;
+				f = check_format(&format[i + 1]);
+				if (f == NULL)
+					return (-1);
+				i += 2;
+				counter += f(ap);
+				continue;
 			}
 		}
 		i++;
 	}
-	va_end(arg);
-	return (displayed);
+	va_end(ap);
+	return (counter);
 }
